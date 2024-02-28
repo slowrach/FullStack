8337b7af312ef20d9aa56cfe9fd45e5b991b46f3
Raquel Queiroz <113996452+slowrach@users.noreply.github.com>
2024-02-28 08:31:30 -0300

    alteração de if/else para estrutura switch

--- STAGE 04.2/script.js
+++ STAGE 04.2/script.js
@@ -1,9 +1,4 @@
-let option = Number(prompt(`Olá, usuário! Digite o número da opção desejada:
-
-1. Cadastrar um item na lista
-2. Mostrar itens cadastrados
-3. Sair do programa`))
-
+let option
 let items = [];
 
 while (option != 3) {
@@ -13,19 +8,23 @@ while (option != 3) {
    2. Mostrar itens cadastrados
    3. Sair do programa`))
 
-   if (option == 1) {
-      newItem = prompt("Digite o item que deseja cadastrar")
+   switch (option) {
+   case 1:
+      let newItem = prompt("Digite o item que deseja cadastrar")
 
       items.push(newItem)
-   } else if (option == 2) {
+      break;
+   case 2:
       if (items.length == 0) {
          alert("Ainda não há itens cadastrados")
       } else {
          alert(items)
       }
-   } else {
+      break;
+   case 3:
       alert("Você saiu do programa")
+      break;
+   default: 
+      alert("Essa opção não é válida")
    }
 }
-
-alert(items)
