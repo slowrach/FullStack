e5a4959988784aa9ce5cd363cac0f7ac0126425f
Raquel Queiroz <113996452+slowrach@users.noreply.github.com>
2024-03-21 09:59:29 -0300

    estilização das duas páginas e início do javascript

--- STAGE 04.2/jogo da advinhação/index.js
+++ STAGE 04.2/jogo da advinhação/index.js
@@ -1,20 +1,21 @@
-alert("JOGO DA ADVINHAÇÃO - TENTE ADVINHAR O NÚMERO!");
-
-let number = Number(prompt("Digite um número de 1 a 10:"));
-
-const randomNumber = Math.ceil(Math.random() * 10);
-
-let attempts = 1;
-
-while (number != randomNumber) {
-  number = prompt("Errou! Tente novamente: ")
-  attempts++
+function attempt() {
+  const kick = document.querySelector("#kick")
+  console.log(kick.value)
 }
 
-const message = `Parabéns, era o número ${randomNumber} mesmo! Você acertou após ${attempts} tentativa`
+// const randomNumber = Math.ceil(Math.random() * 10);
 
-if (attempts == 1) {
-  alert(message)
-} else {
-  alert(message + 's')
-}
+// let attempts = 1;
+
+// while (number != randomNumber) {
+//   number = prompt("Errou! Tente novamente: ")
+//   attempts++
+// }
+
+// const message = `Parabéns, era o número ${randomNumber} mesmo! Você acertou após ${attempts} tentativa`
+
+// if (attempts == 1) {
+//   alert(message)
+// } else {
+//   alert(message + 's')
+// }
