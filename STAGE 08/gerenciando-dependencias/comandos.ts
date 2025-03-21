90d7ec8725d26e29adf173596114135fd20a031d
Raquel Queiroz <113996452+slowrach@users.noreply.github.com>
2025-03-21 10:40:24 -0300

    estudos sobre gerenciamentos de dependências

---
+++ STAGE 08/gerenciando-dependencias/comandos.ts
@@ -0,0 +1,10 @@
+// "npm outdated" ou "npm out" mostra quais são as versões dos pacotes que temos e quais são as recomendadas, para que você possa atualizá-los
+
+// "npx npm-check-updates" também mostra quais são as versões dos pacotes que temos e quais são as recomendadas, para que você possa atualizá-los
+
+// "npx npm-check-updates -u" atualiza todos os pacotes que temos para as versões recomendadas
+
+// "npx npm-check-updates --interactive --format group" serve para você selecionar, de modo interativo, quais pacotes desatualizados você quer atualizar
+
+// "npm i (nome do pacote)@latest" baixa a última versão do pacote disponível
+
